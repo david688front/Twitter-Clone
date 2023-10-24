3dbcfddaae7da524375b3ce19855e40d3456779f
David <d888eightsix@icloud.com>
2023-10-24 10:12:19 +0700

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,36 @@
+import {HomeIcon} from "@heroicons/react/outline"
+
+export default function Sidebar(){
+    return(
+        <div className="hidden sm:flex flex-col fixed">
+            <div>Logo</div>
+            <nav>
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+
+                {/* <SidebarLink text={"Explore"} />
+                <SidebarLink text={"Notifications"} />
+                <SidebarLink text={"Messages"} /> */}
+
+
+
+            </nav>
+            <div>User</div>
+        </div>
+    )
+}
+
+function SidebarLink({text, Icon}){
+    return (
+        <li className="flex mb-6 items-center text-xl space-x-3">
+            <Icon className="h-7"/>
+            <span className="hidden xl:inline">{text}</span>
+            
+        </li>
+    )
+}
