d9866a96bebe238dcd32e6f6e194bd0795fb2084
David <d888eightsix@icloud.com>
2023-11-03 15:38:28 +0700

    3 com

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,32 @@ import styles from "@/styles/Home.module.css";
 import Sidebar from "@/components/Sidebar";
 import PostFeed from "@/components/Postfeed";
 import Trending from "@/components/Trending";
-//import BottomBanner from "@/components/BottomBanner";
-//import { useSelector } from "react-redux";
-//import CoomentModal from "@/components/modals/CommentModal";
+import BottomBanner from "@/components/BottomBanner";
+import { useSelector } from "react-redux";
+import CoomentModal from "@/components/modals/CommentModal";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  // const username = useSelector(state => state.user.username)
-  // console.log(username)
+  const username = useSelector(state => state.user.username)
+  console.log(username)
 
-  const username = "Elon"
+
   return (
     <div>
-      <div
-        className="bg-black min-h-screen text-[#E7E9EA] 
-    max-w-[1400px] mx-auto flex
+        <div
+        className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto flex">
 
-    
-    "
-      >
         <Sidebar />
         <PostFeed />
         <Trending />
       </div>
 
-      {/* <CoomentModal /> */}
+      <CoomentModal />
 
-     {/* {!username && <BottomBanner />} */}
+      {!username && <BottomBanner />}
+   
     </div>
   );
 }
